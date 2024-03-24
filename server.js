f68bbdbe6596b40c39adf635d88d4ad53a04ab91
Jitendra Rawat <120551529+jitendra-rawat@users.noreply.github.com>
2024-03-24 11:09:39 +0530

    add db

--- server.js
+++ server.js
@@ -1,67 +1,56 @@
-import express from 'express'
-import mongoose from 'mongoose';
-import { configDotenv } from 'dotenv';
-import authRoutes from './src/routes/auth.js'
-import tourRoutes from './src/routes/tour.js'
-import messageRoutes from './src/routes/message.js'
-import bookingRoutes from './src/routes/booking.js'
-import blogRoutes from './src/routes/blog.js'
-import cors from 'cors'
+import express from "express";
+import mongoose from "mongoose";
+import { configDotenv } from "dotenv";
+import authRoutes from "./src/routes/auth.js";
+import tourRoutes from "./src/routes/tour.js";
+import messageRoutes from "./src/routes/message.js";
+import bookingRoutes from "./src/routes/booking.js";
+import blogRoutes from "./src/routes/blog.js";
+import cors from "cors";
 
-import productRoutes from './src/routes/product.js'
+import productRoutes from "./src/routes/product.js";
 
-
-
-
-const PORT =  4000;
+const PORT = 4000;
 const app = express();
-configDotenv(); 
+configDotenv();
 
 const corsOptions = {
-  origin: 'http://localhost:4000,https://busybootstravel.netlify.app',
-  methods: ['GET', 'POST','PATCH','PUT','DELETE'], 
-  allowedHeaders: ['Content-Type', 'Authorization'], 
+  origin: "http://localhost:4000,https://busybootstravel.netlify.app",
+  methods: ["GET", "POST", "PATCH", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 
 app.use(cors(corsOptions));
 
-
-
 //connect db
-mongoose.connect('mongodb://localhost:27017/bbt').then( () => console.log("DB Connected")).catch( (err) => console.log(err) )
-
-
-
+mongoose
+  .connect(
+    "mongodb+srv://jitendrasingh:jitendrasingh@cluster0.xygxhgr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("DB Connected"))
+  .catch((err) => console.log(err));
 
 //middlewares
 
 app.use(express.json());
 
-
-
-
 //routes
 
+app.use("/", (req, res) => {
+  res.send("Server is Working");
+});
 
-app.use('/', (req,res) => {
-  res.send("Server is Working")
-})
+app.use("/auth", authRoutes);
 
-app.use('/auth', authRoutes);
+app.use("/tour", tourRoutes);
 
-app.use('/tour',tourRoutes)
+app.use("/message", messageRoutes);
 
-app.use('/message',messageRoutes)
+app.use("/booking", bookingRoutes);
 
-app.use('/booking',bookingRoutes)
+app.use("/blog", blogRoutes);
 
-app.use('/blog',blogRoutes)
-
-
-app.use('/product', productRoutes)
-
-
-
+app.use("/product", productRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
